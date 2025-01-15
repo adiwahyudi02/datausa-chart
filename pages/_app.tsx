60842135c63cc124a490518833e7cae412bf50d2
Adi Wahyudi <adi.whyd02@gmail.com>
2025-01-16 00:05:51 +0700

    feat: setup react-query configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
+import { queryClient } from "@/utils/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ToastContainer />
+    </QueryClientProvider>
+  );
 }
