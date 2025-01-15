6d9b88dba03a35d29838387310c42f3446cfa93f
Adi Wahyudi <adi.whyd02@gmail.com>
2025-01-16 01:21:37 +0700

    feat: add title and meta to the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,24 @@ import "@/styles/globals.css";
 import { queryClient } from "@/utils/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { ToastContainer } from "react-toastify";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Head>
+        <title>
+          American Community Survey (ACS) - U.S. Census Bureau | Adi Wahyudi
+        </title>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="An annual survey by the U.S. Census Bureau providing vital data for communities and planning."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
       <ToastContainer />
     </QueryClientProvider>
